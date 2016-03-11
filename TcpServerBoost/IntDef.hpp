5450907265e108b8912acc3a33f10dc059a17ed1
zhouyu0615 <871211719@qq.com>
2016-03-11 09:04:03 +0800

    Create

---
+++ TcpServerBoost/IntDef.hpp
@@ -0,0 +1,12 @@
+//IntDef.hpp
+
+#include <boost/cstdint.hpp>
+
+using boost::int8_t;
+using boost::uint8_t;
+using boost::int16_t;
+using boost::uint16_t;
+using boost::int32_t;
+using boost::uint32_t;
+using boost::int64_t;
+using boost::uint64_t;
